c96a20369da0450a896d7946ef3292584f353b1f
nilskrannig <nils.krannig@gmx.de>
2024-04-20 20:55:35 +0200

    Part4_Exercises: Helper Functions and Unit Tests, Step 2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,17 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const initialValue = 0
+    const sum = blogs.reduce(
+        (acc, value) => acc + value.likes,
+        initialValue
+    )
+
+    return sum
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
